90dacdc275bad7c9101c9502cd520a18a64cd5bc
Avaz <avazbekrozimboyev08@gmail.com>
2024-12-05 23:41:42 +0500

    figma

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        white: "#fff",
+        aaac11c: "#a6331c",
+        o200: "#cc0200",
+        bleck: "#000",
+        fb: "#f6f7fb",
+        brack: "#000",
+      },
+      fontFamily: {
+        sans: ["Abel"],
+      },
+    },
+  },
+  plugins: [],
+};
